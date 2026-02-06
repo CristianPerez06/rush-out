46e4fc6a66a28e7914935ff46395a483d93c1d32
jiscocozza <jiscocozza@gmail.com>
2026-02-06 15:02:19 -0300

    Implement navigation on video end in Intro component

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom'
 import splashVideo from '../assets/splash.mp4'
 
 const Intro = () => {
+  const navigate = useNavigate()
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-black text-white">
       <video
@@ -8,7 +10,7 @@ const Intro = () => {
         autoPlay
         muted
         playsInline
-        loop
+        onEnded={() => navigate('/')}
         className="max-h-[70vh] w-full max-w-4xl object-contain"
       />
     </div>

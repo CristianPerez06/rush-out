ac7c2c1382398c0b5b9e5dbe226da40337a5856f
jiscocozza <jiscocozza@gmail.com>
2026-02-06 14:50:38 -0300

    Added splash video to Intro component and updated layout for video playback.

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,8 +1,16 @@
+import splashVideo from '../assets/splash.mp4'
+
 const Intro = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold">Intro Video</h1>
-      {/* TODO: Add intro video player here */}
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-black text-white">
+      <video
+        src={splashVideo}
+        autoPlay
+        muted
+        playsInline
+        loop
+        className="max-h-[70vh] w-full max-w-4xl object-contain"
+      />
     </div>
   )
 }
